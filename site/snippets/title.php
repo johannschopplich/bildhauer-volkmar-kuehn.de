9acce8e1552567f631329a7bf3e49db11aacb4c4
Johann Schopplich <mail@johannschopplich.com>
2023-04-05 20:53:00 +0200

    fix: custom title classes

--- site/snippets/title.php
+++ site/snippets/title.php
@@ -2,6 +2,6 @@
 /** @var string $title */
 /** @var string|null $props */
 ?>
-<h1 class="<?= trim('relative text-contrast-higher font-heading text-2xl md:text-3xl' . ($props ?? ''), ' ') ?>">
+<h1 class="<?= trim('relative text-contrast-higher font-heading text-2xl md:text-3xl ' . ($props ?? ''), ' ') ?>">
   <?= $title ?>
 </h1>
