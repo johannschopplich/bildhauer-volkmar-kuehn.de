18654ff2930d9f802e2ce656a6c57cf4389c26ab
Johann Schopplich <mail@johannschopplich.com>
2023-04-06 15:33:16 +0200

    feat: improve title design

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,12 +5,14 @@
 snippet('layouts/default', slots: true);
 ?>
 
-<div class="py-4xl">
-  <div class="max-w-screen-sm mx-lg sm:mx-3xl">
-    <div class="mb-5xl">
+<div class="pb-4xl">
+  <div class="w-min border-b-2 border-primary-700 py-3xl mb-5xl">
+    <div class="max-w-screen-sm ml-lg sm:ml-3xl">
       <?php snippet('title', ['title' => $page->title()->escape()]) ?>
     </div>
+  </div>
 
+  <div class="max-w-screen-sm mx-lg sm:mx-3xl">
     <?php $blocks = $page->text()->toBlocks() ?>
     <?php if ($blocks->isNotEmpty()): ?>
       <div class="mb-5xl">
