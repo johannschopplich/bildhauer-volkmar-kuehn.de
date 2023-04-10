8fb94242d0f4e86779dcbb8052678e3ae76f61f5
Johann Schopplich <mail@johannschopplich.com>
2023-04-10 19:13:20 +0200

    fix: home page gallery

--- site/templates/home.php
+++ site/templates/home.php
@@ -15,32 +15,33 @@ snippet('layouts/default', slots: true);
   <div class="max-w-screen-sm mx-lg sm:mx-3xl">
     <?php $blocks = $page->text()->toBlocks() ?>
     <?php if ($blocks->isNotEmpty()): ?>
-      <div class="mb-5xl">
-        <section class="prose md:column-count-2">
-          <?= $blocks ?>
-        </section>
-      </div>
+      <section class="prose md:column-count-2">
+        <?= $blocks ?>
+      </section>
     <?php endif ?>
   </div>
 
-  <div class="mx-1 md:mx-3xl">
-    <div class="masonry-grid gap-1 md:gap-2" style="--masonry-col-max-w: clamp(10rem, 25vw, 15rem);">
-      <?php foreach ($page->gallery()->toFiles() as $image): ?>
-        <?php /** @var \Kirby\Cms\File $image */ ?>
-        <figure>
-          <?php snippet('helpers/image', [
-            'image' => $image
-          ]) ?>
+  <?php $images = $page->gallery()->toFiles() ?>
+  <?php if ($images->isNotEmpty()): ?>
+    <div class="mx-1 mt-5xl md:mx-3xl">
+      <div class="masonry-grid gap-1 md:gap-2" style="--masonry-col-max-w: clamp(10rem, 25vw, 15rem);">
+        <?php foreach ($page->gallery()->toFiles() as $image): ?>
+          <?php /** @var \Kirby\Cms\File $image */ ?>
+          <figure>
+            <?php snippet('helpers/image', [
+              'image' => $image
+            ]) ?>
 
-          <?php if ($image->caption()->isNotEmpty() && !$page->isHomePage()): ?>
-            <figcaption class="text-sm leading-normal text-center px-md py-xs">
-              <?= $image->caption() ?>
-            </figcaption>
-          <?php endif ?>
-        </figure>
-      <?php endforeach ?>
+            <?php if ($image->caption()->isNotEmpty() && !$page->isHomePage()): ?>
+              <figcaption class="text-sm leading-normal text-center px-md py-xs">
+                <?= $image->caption() ?>
+              </figcaption>
+            <?php endif ?>
+          </figure>
+        <?php endforeach ?>
+      </div>
     </div>
-  </div>
+  <?php endif ?>
 </div>
 
 <?php endsnippet() ?>
