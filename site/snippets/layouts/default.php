d5a54fb2ecac17ddc14b9f0bdc4b25ddf750dd3d
Johann Schopplich <mail@johannschopplich.com>
2026-02-10 10:07:18 +0100

    chore: update primary color palette

--- site/snippets/layouts/default.php
+++ site/snippets/layouts/default.php
@@ -19,7 +19,7 @@
   <?= $meta->jsonld() ?>
   <?= $meta->social() ?>
 
-  <meta name="theme-color" content="#524231">
+  <meta name="theme-color" content="#625240">
   <link rel="icon" href="/assets/images/favicon.svg" type="image/svg+xml">
   <link rel="manifest" href="/manifest.json">
   <link rel="apple-touch-icon" href="/assets/images/apple-touch-icon.png">
